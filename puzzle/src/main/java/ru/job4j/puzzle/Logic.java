38181f9c902d26c52eec63a24c7d778ca18e82ff
Mirto111 <mitritdil@ya.ru>
2020-04-14 15:16:56 +0300

    2. Сокобан. Тестовое задание.[#255792]

--- puzzle/src/main/java/ru/job4j/puzzle/Logic.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Logic.java
@@ -1,9 +1,8 @@
 package ru.job4j.puzzle;
 
+import java.util.Arrays;
 import ru.job4j.puzzle.firuges.Cell;
 import ru.job4j.puzzle.firuges.Figure;
-
-import java.util.Arrays;
 
 /**
  * //TODO add comments.
@@ -13,82 +12,122 @@ import java.util.Arrays;
  * @since 0.1
  */
 public class Logic {
-    private final int size;
-    private final Figure[] figures;
-    private int index = 0;
 
-    public Logic(int size) {
-        this.size = size;
-        this.figures = new Figure[size * size];
+  private final int size;
+  private final Figure[] figures;
+  private int index = 0;
+
+  public Logic(int size) {
+    this.size = size;
+    this.figures = new Figure[size * size];
+  }
+
+  private static boolean monoHorizontal(int[][] board, int row) {
+    boolean result = true;
+    for (int i = 0; i < board[row].length; i++) {
+      if (board[row][i] != 1) {
+        result = false;
+        break;
+      }
     }
+    return result;
+  }
 
-    public void add(Figure figure) {
-        this.figures[this.index++] = figure;
+  private static boolean monoVertical(int[][] board, int column) {
+    boolean result = true;
+    for (int i = 0; i < board.length; i++) {
+      if (board[i][column] != 1) {
+        result = false;
+        break;
+      }
     }
+    return result;
+  }
 
-    public boolean move(Cell source, Cell dest) {
-        boolean rst = false;
-        int index = this.findBy(source);
-        if (index != -1) {
-            Cell[] steps = this.figures[index].way(source, dest);
-            if (this.isFree(steps)) {
-                rst = true;
-                this.figures[index] = this.figures[index].copy(dest);
-            }
+  private static int[] extractDiagonal(int[][] board) {
+    int[] rsl = new int[board.length];
+    for (int i = 0; i < board.length; i++) {
+      rsl[i] = board[i][i];
+    }
+    return rsl;
+  }
+
+  public void add(Figure figure) {
+    this.figures[this.index++] = figure;
+  }
+
+  public boolean move(Cell source, Cell dest) {
+    boolean rst = false;
+    int index = this.findBy(source);
+    if (index != -1) {
+      Cell[] steps = this.figures[index].way(source, dest);
+      if (this.isFree(steps)) {
+        rst = true;
+        this.figures[index] = this.figures[index].copy(dest);
+      }
+    }
+    return rst;
+  }
+
+  public boolean isFree(Cell... cells) {
+    boolean result = cells.length > 0;
+    for (Cell cell : cells) {
+      if (this.findBy(cell) != -1) {
+        result = false;
+        break;
+      }
+    }
+    return result;
+  }
+
+  public void clean() {
+    for (int position = 0; position != this.figures.length; position++) {
+      this.figures[position] = null;
+    }
+    this.index = 0;
+  }
+
+  private int findBy(Cell cell) {
+    int rst = -1;
+    for (int index = 0; index != this.figures.length; index++) {
+      if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
+        rst = index;
+        break;
+      }
+    }
+    return rst;
+  }
+
+  public boolean isWin() {
+    int[][] table = this.convert();
+    boolean result = false;
+    int[] diagonal = extractDiagonal(table);
+    for (int i = 0; i < diagonal.length; i++) {
+      if (diagonal[i] == 1) {
+        if (monoVertical(table, i) || monoHorizontal(table, i)) {
+          result = true;
+          break;
         }
-        return rst;
+      }
     }
+    return result;
+  }
 
-    public boolean isFree(Cell ... cells) {
-        boolean result = cells.length > 0;
-        for (Cell cell : cells) {
-            if (this.findBy(cell) != -1) {
-               result = false;
-               break;
-            }
+  public int[][] convert() {
+    int[][] table = new int[this.size][this.size];
+    for (int row = 0; row != table.length; row++) {
+      for (int cell = 0; cell != table.length; cell++) {
+        int position = this.findBy(new Cell(row, cell));
+        if (position != -1 && this.figures[position].movable()) {
+          table[row][cell] = 1;
         }
-        return result;
+      }
     }
+    return table;
+  }
 
-    public void clean() {
-        for (int position = 0; position != this.figures.length; position++) {
-            this.figures[position] = null;
-        }
-        this.index = 0;
-    }
-
-    private int findBy(Cell cell) {
-        int rst = -1;
-        for (int index = 0; index != this.figures.length; index++) {
-            if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
-                rst = index;
-                break;
-            }
-        }
-        return rst;
-    }
-
-    public boolean isWin() {
-        int[][] table = this.convert();
-        boolean result = false;
-        return result;
-    }
-
-    public int[][] convert() {
-        int[][] table = new int[this.size][this.size];
-        for (int row = 0; row != table.length; row++) {
-            for (int cell = 0; cell != table.length; cell++) {
-                int position = this.findBy(new Cell(row, cell));
-                if (position != -1 && this.figures[position].movable()) {
-                    table[row][cell] = 1;
-                }
-            }
-        }
-        return table;
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(this.convert());
-    }
+  @Override
+  public String toString() {
+    return Arrays.toString(this.convert());
+  }
 }
